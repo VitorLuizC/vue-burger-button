297ebd886d4104426df15c25124c9c36ee729ee3
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-19 19:36:07 -0300

    :sparkles: create `start` script demo

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <BurgerButton
+    :active="isActive"
+    :bar-width="80"
+    :bar-height="10"
+    @click="() => isActive = !isActive"
+  />
+</template>
+
+<script>
+  import BurgerButton from './BurgerButton.vue';
+
+  export default {
+    components: { BurgerButton },
+    data: () => ({ isActive: false })
+  };
+</script>
