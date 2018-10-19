390640811466536cbf2e9e609e382c256e48bcba
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-19 19:35:31 -0300

    :sparkles: create BurgerButton component

---
+++ src/BurgerButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    :class="['BurgerButton', { 'BurgerButton--active': active }]"
+    aria-label="Menu"
+    aria-controls="navigation"
+    :aria-expanded="active ? 'true' : 'false'"
+    @click.prevent="$emit('click', $event)"
+    :style="{ minWidth: barWidth + 'px', minHeight: barHeight * 4.5 + 'px' }"
+  >
+    <span
+      v-for="index in 3"
+      :key="index"
+      :style="{ width: barWidth + 'px', height: barHeight + 'px' }"
+      class="BurgerButton__bar"
+    />
+  </button>
+</template>
+
+<script>
+  const px = (value) => String(value) + 'px'
+
+  export default {
+    name: 'BurgerButton',
+    props: {
+      active: Boolean,
+      barWidth: {
+        type: Number,
+        default: 100
+      },
+      barHeight: {
+        type: Number,
+        default: 20
+      }
+    }
+  };
+</script>
+
+<style src="./BurgerButton.styl" lang="stylus" />
