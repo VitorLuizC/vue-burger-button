621bc143e0d5c81b19f035b6932f14307bef2b2f
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-20 13:56:42 -0300

    :truck: move demo sources to examples/

---
+++ examples/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <p>Using default bar width and height.</p>
+
+    <BurgerButton
+      :active="isActive"
+      @click="() => isActive = !isActive"
+    />
+
+    <BurgerButton
+      :active="!isActive"
+      @click="() => isActive = !isActive"
+    />
+  </section>
+</template>
+
+<script>
+  import BurgerButton from '../src/BurgerButton.vue';
+
+  export default {
+    components: { BurgerButton },
+    data: () => ({ isActive: false })
+  };
+</script>
