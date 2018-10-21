87be9b3e8e445d32fed433e35946af3096f1ef2e
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-21 00:14:19 -0300

    :wrench: configure package entries to module bundles

--- examples/App.vue
+++ examples/App.vue
@@ -15,10 +15,12 @@
 </template>
 
 <script>
-  import BurgerButton from '../src/BurgerButton.vue';
+  import BurgerButton from '../';
 
   export default {
     components: { BurgerButton },
     data: () => ({ isActive: false })
   };
 </script>
+
+<style src="../dist/vue-burguer-button.css" />
