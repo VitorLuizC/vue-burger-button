918ad80cb8b54d0eae7b0d3cf07f2f221371ff3d
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-21 00:05:52 -0300

    :package: setups build with Bili

---
+++ bili.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { name } = require('./package.json');
+const vue = require('rollup-plugin-vue').default;
+const css = require('rollup-plugin-css-only');
+
+module.exports = {
+  input: 'src/BurgerButton.vue',
+  banner: true,
+  format: ['umd', 'umd-min', 'cjs', 'es'],
+  filename: `${name}[suffix].js`,
+  plugins: [
+    vue({ css: false }),
+    css({ output: `dist/${name}.css` }),
+  ]
+};
