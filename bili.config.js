33ede7959a5fef4d3688602b360a573255385a79
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-21 19:41:51 -0300

    :bug: fixes module name on UMD bundle

--- bili.config.js
+++ bili.config.js
@@ -9,6 +9,7 @@ module.exports = {
   banner: true,
   format: ['umd', 'umd-min', 'cjs', 'es'],
   filename: `${name}[suffix].js`,
+  moduleName: 'VueBurgerButton',
   plugins: [
     vue({ css: false }),
     css({ output: `dist/${name}.css` })
